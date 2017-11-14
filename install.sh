259e827fd9c1497bdc9b64d45b7e21dd8dbc830f
vtrbtf <vitor.tfreitas@gmail.com>
2017-11-14 15:45:26 -0200

    WIP

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #/bin/bash
 
-curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
+which docker
+if [ $? -ne 0 ]
+then
+    curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
+fi
+
 sudo apt-get install -y unzip curl
 
-rm -rf log-processor/ master.zip && wget https://github.com/vtrbtf/log-processing-example/archive/master.zip && unzip master.zip -d log-processor
+rm -rf log-processor/ master.zip
+wget https://github.com/vtrbtf/log-processing-example/archive/master.zip 
+unzip master.zip -d log-processor
+
 cd log-processor/log-processing-example-master
 cd input && sudo docker build -t accesslog-generator . && cd ..
 cd collect && sudo docker build -t accesslog-collector . && cd ..
